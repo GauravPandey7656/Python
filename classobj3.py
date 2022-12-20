2bd190cbdedc4c5c9add7239ef2c1d75fc1a9e89
Gaurav Pandey <gauravpandey7656@gmail.com>
2022-12-20 11:36:53 +0530

    classobj3.py

---
+++ classobj3.py
@@ -0,0 +1,27 @@
+class A:
+#     def __init__(self):
+#         print("init of class A")
+#     def method1(self):
+#         print("This is method 1")
+#     def method2(self):
+#         print("This is method 2")
+# class B(A):
+#     def __init__(self):
+#         super().__init__()
+#         print("init of class B")
+#     def method3(self):
+#         print("This is method 3")
+#     def method4(self):
+#         print("This is method 4")
+# class C(B):
+#     def __init__(self):
+#         super().__init__()
+#         print("init of class C")
+#     def method5(self):
+#         print("This is method 5")
+# obj=C()
+def add(x,y,z=0):
+    return x+y+z
+print(def(3,4))
+print(def(3,4,5))
+
