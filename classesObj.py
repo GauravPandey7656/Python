715dd72eddf4c54723c62ddc1fc4c5238c721ae5
Gaurav Pandey <gauravpandey7656@gmail.com>
2022-12-20 11:35:27 +0530

    classesObj.py

---
+++ classesObj.py
@@ -0,0 +1,11 @@
+class laptop:
+    def __init__(self):
+        print("Hello World")
+    def config(self):
+        print("Asus","i7","1TB")
+laptop1=laptop()
+laptop2=laptop()
+# laptop.config(laptop1)
+laptop1.config()
+laptop2.config()
+
