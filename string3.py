6745a45462df6ffc31cc9f82ede901207ec71769
Gaurav Pandey <gauravpandey7656@gmail.com>
2023-01-06 15:35:14 +0530

    string3.py

---
+++ string3.py
@@ -0,0 +1,6 @@
+string=input("Enter a string:")  #starting index is inclusive and last index is exclusive
+print(string[::-1]) #Reverse
+print(string[0:11]) #from 0 index to index value 10
+print(string[:10])  #from 0 index to index value 9
+print(string[5:])   #from 5 index to last index value
+print(string[0::2])  #from index 0 to last index value and leaving 1 character in between
